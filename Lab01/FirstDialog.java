100ae9d9658fb451d71760513b30f927e6f4ff77
theRealLasHiD <thanh.pv.ds@gmail.com>
2021-03-09 15:08:21 +0700

    First Commit

---
+++ Lab01/FirstDialog.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class FirstDialog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Hello World! How are you?");
+		System.exit(0);
+	}
+
+}
---
+++ Lab01/HelloWorld.java
@@ -0,0 +1,10 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Xin chao \n cac ban");
+		System.out.println("Hello \t Wolrd");
+	}
+
+}
