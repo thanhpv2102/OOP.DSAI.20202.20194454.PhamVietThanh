30f16c9d662331a25ac12c7dc3c5e5ee17e7d11d
theRealLasHiD <thanh.pv.ds@gmail.com>
2021-03-09 16:41:39 +0700

    Second commit

--- Lab01/NStars.java
+++ Lab01/NStars.java
@@ -7,28 +7,21 @@ public class NStars {
 		Scanner scanner = new Scanner(System.in);
 		System.out.println("Input n");
 		int n = scanner.nextInt();
-		
-		String result;
-		int incr = 1;
-		int current = 1;
-		for (int i = 1; i <= n; i++) {
-			if (i % 2 == 1) {
-				incr++;
-				result = "";
-				for (int j = 1; j <= current + incr; j++) {
-					result += "*";
-				}
-				System.out.println(result);
-				
-			} else {
-				result = "";
-				for (int j = 1; j <= current + incr; j++) {
-					result += "*";
-				}
-				System.out.println(result);
+
+
+		int i, j, k;
+		for(i = 1; i <= n; i++)
+		{
+			for(j = n; j > i; j--)
+			{
+				System.out.print(" ");
 			}
-			current += incr;
+			for(k = 1; k < (i * 2); k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
-
 }
+
