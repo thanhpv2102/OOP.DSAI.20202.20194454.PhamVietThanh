164834be150e93f73b52d9a78c81fce1b86ba561
theRealLasHiD <thanh.pv.ds@gmail.com>
2021-04-08 23:47:12 +0700

    update Store class and StoreTest

--- AimsProject/src/hust/soict/dsai/aims/store/Store.java
+++ AimsProject/src/hust/soict/dsai/aims/store/Store.java
@@ -14,6 +14,8 @@ public class Store {
 	public void addDVD(DigitalVideoDisc disc) {
 		if (qty == MAX_NUMBERS_INSTOCK) {
 			System.out.println("Store full");
+		} else if (isInStore(disc)){
+			System.out.println(disc.getTitle() + " already in store");
 		} else {
 			itemsInStore[qty] = disc;
 			System.out.println("Successfully added " + itemsInStore[qty].getTitle());
@@ -22,36 +24,49 @@ public class Store {
 	}
 	
 	public void removeDVD(DigitalVideoDisc disc) {
-		System.out.println("Successfully removed DVD " + disc.getTitle());
-		java.util.List<DigitalVideoDisc> temp = new ArrayList<DigitalVideoDisc>(Arrays.asList(itemsInStore));
-		temp.remove(disc);
-		itemsInStore = temp.toArray(itemsInStore);
-		qty -= 1;
+		if (isInStore(disc)) {
+			System.out.println("Successfully removed DVD " + disc.getTitle());
+			java.util.List<DigitalVideoDisc> temp = new ArrayList<DigitalVideoDisc>(Arrays.asList(itemsInStore));
+			temp.remove(disc);
+			itemsInStore = temp.toArray(itemsInStore);
+			qty -= 1;
+		} else {
+			System.out.println(disc.getTitle() + " not in store");
+		}
+	}
+	
+	public void removeDVD(DigitalVideoDisc[] dvdList) {
+		for (int i = 0; i < dvdList.length; i++) {
+			if (isInStore(dvdList[i])) {
+				System.out.println("Successfully removed DVD " + dvdList[i].getTitle());
+				java.util.List<DigitalVideoDisc> temp = new ArrayList<DigitalVideoDisc>(Arrays.asList(itemsInStore));
+				temp.remove(dvdList[i]);
+				itemsInStore = temp.toArray(itemsInStore);
+				qty -= 1;
+			} else {
+				System.out.println(dvdList[i].getTitle() + " not in store");
+			}
+		}
 	}
 
 	public void addDVD(DigitalVideoDisc[] dvdList) {
 		if (qty == MAX_NUMBERS_INSTOCK || qty + dvdList.length > MAX_NUMBERS_INSTOCK) {
 			System.out.println("Store full");
 		} else {
-			for (int i = 1; i <= dvdList.length; i++) {
-				itemsInStore[qty + i - 1] = dvdList[i-1];
+			int count = 0;
+			for (int i = 0; i < dvdList.length; i++) {
+				if (!isInStore(dvdList[i])) {
+					itemsInStore[qty + count] = dvdList[i];
+					count++;
+				} else {
+					System.out.println(dvdList[i].getTitle() + " already in store");
+				}
 			}
-			System.out.println("Successfully added " + dvdList.length + " items");
-			qty += dvdList.length;
+			System.out.println("Successfully added " + count + " items");
+			qty += count;
 		}
 	}
 
-	public void addDVD(DigitalVideoDisc dvd1, DigitalVideoDisc dvd2) {
-		if (qty == MAX_NUMBERS_INSTOCK || qty + 2 > MAX_NUMBERS_INSTOCK) {
-			System.out.println("Store full");
-		} else {
-			itemsInStore[qty] = dvd1;
-			itemsInStore[qty+1] = dvd2;
-			System.out.println("Successfully added 2 items");
-			qty += 2;
-		}
-	}
-	
 	public void print() {
 		System.out.println("***********************STORE***********************");
 		System.out.println("Items:");
@@ -60,5 +75,14 @@ public class Store {
 		}
 		System.out.println("***************************************************");
 
+	}
+	
+	private boolean isInStore(DigitalVideoDisc disc) {
+		for (int i = 0; i < qty; i++) {
+			if (itemsInStore[i].equals(disc)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
--- AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
+++ AimsProject/src/hust/soict/dsai/test/store/StoreTest.java
@@ -6,7 +6,7 @@ import hust.soict.dsai.aims.store.Store;
 public class StoreTest {
 
 	public static void main(String[] args) {
-		//Create a new cart
+		//Create a new store
 		Store store = new Store();
 
 		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
@@ -17,8 +17,17 @@ public class StoreTest {
 				"Animation", 18.99f);
 
 		store.addDVD(dvd1);
-		store.addDVD(dvd2, dvd3);
+		
+		//adding duplicate DVD will show warning and not add that DVD.
+		DigitalVideoDisc[] dvdList = {dvd1, dvd2, dvd3};
+		store.addDVD(dvdList);
 
+		
+		store.removeDVD(dvd3);
+		//removing item not in array will show warning
+		DigitalVideoDisc dvd4 = new DigitalVideoDisc("YEYEYE",
+				"Animation", 18.99f);
+		store.removeDVD(dvd4);
 		//Test the print method
 		store.print();
 
